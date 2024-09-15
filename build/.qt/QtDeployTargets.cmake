d9a6be8d8e22477994a25450da3fdd8101728c4d
kprado <kprad7571@gmail.com>
2024-09-15 00:50:34 -0500

    build files untracked

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Tetris_FILE /home/monac/Projects/C++/Tetris/build/Tetris)
-set(__QT_DEPLOY_TARGET_Tetris_TYPE EXECUTABLE)
--- build/CMakeFiles/Tetris.dir/Tetris_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Tetris.dir/Tetris_autogen/mocs_compilation.cpp.o: \
- /home/monac/Projects/C++/Tetris/build/Tetris_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
