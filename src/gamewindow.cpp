35d5204e1c5038cee67bcd72cb5711bec91f7aaf
KPrado <kprad7571@gmail.com>
2024-10-17 20:35:13 -0500

    feat: Game window with tetromino

--- src/gamewindow.cpp
+++ src/gamewindow.cpp
@@ -1,40 +1,55 @@
 #include "gamewindow.h"
 
-std::string GetTetroidShape() {
-	/*
-	 * Randomly selects a tetroid shape for game to draw
-	 * TODO - Potenitally check for tetroid repitition to keep true(?) randomisation
-	 */
-	// int type cast handles the rounding
-	int shape = std::rand() % 8;
-	std::string tetroidShape;
-	switch(shape){
-		case 1:
-			tetroidShape = "~/lib/Tetroids/Cube.png";
-			break;
-		case 2:
-			tetroidShape = "~/lib/Tetroids/L-Inverted.png";
-			break;
-		case 3:
-			tetroidShape = "~/lib/Tetroids/L-R_Zigzag.png";
-			break;
-		case 4:
-			tetroidShape = "~/lib/Tetroids/L-Shape.png";
-			break;
-		case 5:
-			tetroidShape = "~/lib/Tetroids/Long.png";
-			break;
-		case 6:
-			tetroidShape = "~/lib/Tetroids/R-L_Zigzag.png";
-			break;
-		case 7:
-			tetroidShape = "~/lib/Tetroids/T-Shape.png";
-			break;
+Game::GameWindow::GameWindow(QWidget* parent) :
+	QWidget(parent) {
+		setGeometry(10, 10, 200, 480);
+		tetromino = new Tetroid();
+		tetromino->show(); // TEST ONLY, DO NOT ACTUALLY KEEP THIS, FUTURE ME
 	}
-	return tetroidShape;
-}
 
 Game::Tetroid::Tetroid(QLabel *parent) :
 	QLabel(parent) {
-		TetroidShape = GetTetroidShape();
+		tetroidShape = GetTetroidShape(tetroidShape);
 	}
+
+/******* PRIVATE FUNCTIONS *******/
+
+	QImage* GetTetroidShape(QImage* &tetroidShape) { // BUG - cmake exits; states func is undefined
+	/*
+	 * Randomly selects a tetroid shape for game to draw
+	 * Tetromino rotation calls for img manipulation
+	 * TODO - Potenitally check for tetroid repitition to keep true(?) randomisation
+	 */
+	// int type cast handles the rounding
+		int* shape = new int;
+		*shape = std::rand() % 8;
+		switch(*shape){
+			// pretty sure the compiler won't know how to handle '~'
+			case 1:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/Cube.png"),nullptr);
+				break;
+			case 2:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/L-Inverted.png"),nullptr);
+				break;
+			case 3:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/L-R_Zigzag.png"),nullptr);
+				break;
+			case 4:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/L-Shape.png"),nullptr);
+				break;
+			case 5:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/Long.png"),nullptr);
+				break;
+			case 6:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/R-L_Zigzag.png"),nullptr);
+				break;
+			case 7:
+				*tetroidShape = QImage(QString("~/lib/tetrominoes/T-Shape.png"),nullptr);
+				break;
+		}
+		delete(shape);
+		shape = nullptr;
+		return tetroidShape;
+	}
+
+
--- src/gamewindow.h
+++ src/gamewindow.h
@@ -1,33 +1,29 @@
 #ifndef GAMEWINDOW_H
 #define GAMEWINDOW_H
 
-#include <random>
-#include <string>
+#include <QImage>
 #include <QLabel>
-#include <QPixmap>
 #include <QWidget>
 
-std::string GetTetroidShape();
-
 namespace Game {
-	 class GameWindow {
-		public:
-			explicit GameWindow();
-		private:
-			QWidget *gameWindow;
-			QWidget *nextTetroidWindow;
-			QWidget *savedTetroidWindow;
-			QObject *savedTetroid;
-	};
-
 	class Tetroid : public QLabel {
 		public: 
 			explicit Tetroid(QLabel *parent = 0);
 		private:
-			std::string TetroidShape;
-			int *height;
-			int *width;
+			QImage* tetroidShape;
+			QImage* GetTetroidShape(QImage* &tetroidShape);
+			int* height;
+			int* width;
 	};
 
+
+	 class GameWindow :public QWidget{
+		public:
+			explicit GameWindow(QWidget* parent = 0);
+		private:
+			Tetroid* tetromino;
+			QObject* tetrisLine; //placeholder, type is not yet chosen
+			Tetroid* savedTetromino;
+	};
 }
 #endif
